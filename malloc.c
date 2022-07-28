74b73ae64f78b4ae093b4af7b5266cdbe8910a81
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-07-28 06:01:35 +0200

    create_new_zone

--- malloc.c
+++ malloc.c
@@ -22,6 +22,41 @@ size_t find_block_size(size_t index, size_t zone_size, char *ptr)
 	return index - start;
 }
 
+t_alloc_zones *create_new_zone(char type, t_alloc_sizes as, size_t size)
+{
+	void *ptr;
+
+	if (!allocs_ptr)
+	{
+		if (!(allocs_ptr = allocate(sizeof(t_alloc_zones) + 1)))
+			return (NULL);
+		if (type == 's')
+		{
+			allocs_ptr->available_space = as.small_alloc - (size + sizeof(t_heap_header));
+			if (!(ptr = allocate(as.small_alloc)))
+				return (NULL);
+		}
+		else if (type == 't')
+		{
+			allocs_ptr->available_space = as.tiny_alloc - (size + sizeof(t_heap_header));
+			if (!(ptr = allocate(as.tiny_alloc)))
+				return (NULL);
+		}
+		else if (type == 'l')
+		{
+			allocs_ptr->available_space = 0;
+			if (!(ptr = allocate(size)))
+				return (NULL);
+		}
+		//Common part
+		allocs_ptr->ptr = ptr;
+		*(char *)(allocs_ptr + 1) = '\0';
+		allocs_ptr->type = type;
+		return (allocs_ptr);
+	}
+	//Continue here
+}
+
 //dev note : free will bzero the memory this way we can easily find the next used (1) block.
 void *search_free_slice(size_t size, char *start_ptr, size_t zone_size)
 {
@@ -43,6 +78,7 @@ void *search_free_slice(size_t size, char *start_ptr, size_t zone_size)
 void *small_alloc(size_t size, t_alloc_sizes as)
 {
 	void	*free_zone_ptr;
+	void	*free_slice_ptr;
 
 	if (allocs_ptr) //Let's search for a free spot
 	{
@@ -50,7 +86,9 @@ void *small_alloc(size_t size, t_alloc_sizes as)
 			;//create_new_zone(size)
 		else
 		{
-			search_free_slice(size, free_zone_ptr, as.small_alloc);
+			if ((free_slice_ptr = search_free_slice(size, free_zone_ptr, as.small_alloc)))
+				return (free_slice_ptr);
+			return (NULL);
 		}
 	}
 	else
