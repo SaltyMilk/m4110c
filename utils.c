c3355a0dfc1f6b5217948fc447beafb2cf651e47
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-08-31 11:43:48 +0200

    hmm trying stuff

--- utils.c
+++ utils.c
@@ -15,10 +15,10 @@ void get_sizes(t_alloc_sizes *as)
 {
 	int pagesize = getpagesize();
 
-	as->tiny_alloc = pagesize;
-	as->tiny_limit = (pagesize / 100) - sizeof(t_heap_header);
-	as->small_alloc = pagesize * 1000;
-	as->small_limit = (pagesize * 10) - sizeof(t_heap_header);
+	as->tiny_alloc = pagesize * 10;
+	as->tiny_limit = (pagesize / 10) - sizeof(t_heap_header);
+	as->small_alloc = pagesize * 100;
+	as->small_limit = (pagesize) - sizeof(t_heap_header);
 }
 
 t_alloc_zones *search_free_zone(size_t size, char type)
